bd56ae35fde48cf0571e6a708bbfdd27a99e7a46
Максим Щучкин <maxventilator@gmail.com>
2024-10-08 02:24:22 +0300

    first blood

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <string>
+#include <cassert>
 
-using std::string;
 
-// Пока асбтрактный
+
 class InputAction {
 public:
     virtual ~InputAction() = default; // на всякий случай
-    virtual string getType() const = 0;
+    virtual std::string getType() const = 0;
 protected:
     // smth
 private:
@@ -17,18 +17,24 @@ private:
 
 class KeyboardInput: public InputAction {
 public:
-    KeyboardInput(const string& keyName, double actionTime, const string& actionType, const std::string& controlName)
+    KeyboardInput(const std::string& keyName, double actionTime, const std::string& actionType, const std::string& controlName)
     : keyName_(keyName), actionTime_(actionTime), actionType_(actionType), controlName_(controlName){}
 
-    string getType() const override {
+    //по поводу const string& - Я читал, что это может быть менее эффективно из-за Named Return Value Optimization (NRVO)
+    std::string getType() const override {
         return "KeyboardInput";
     }
+    // const std::string& getType() const override {
+    //     static const std::string type = "KeyboardInput";
+    //     return type;
+    // }
 
-    void show() const {
-        std::cout<< "keyName: " << keyName_ << 
-                    "\nactionTime: " << actionTime_ <<
-                    "\nactionTipe: " << actionType_ <<
-                    "\ncontrolName: " << controlName_;
+
+    std::string show() const {
+        return "keyname: " + keyName_ + "\n" +
+                "actionTime: " + std::to_string(actionTime_) + "\n" +
+                "actionType: " + actionType_ + "\n" +
+                "controlName: " + controlName_ + "\n";
     }
 
     // Создание объекта "отпускание" по объекту "нажатие" и времени отпускания
@@ -37,39 +43,36 @@ public:
     }
 
     bool operator<(const KeyboardInput& other) const {
-        // стоит ли добавить проверкку на равенство keyName? (keyName_ == other.keyName_)
         return actionTime_ < other.actionTime_;
     }
 
     bool operator==(const KeyboardInput& other) const {
-        // стоит ли добавить проверкку на равенство keyName? (keyName_ == other.keyName_)
         return actionTime_ == other.actionTime_;
     }
-
-    //пока обойдемся без spaceship
+    
     
 private:
-    string keyName_;
+    std::string keyName_;
     double actionTime_; // Это не длительность, а именно время!
-    string actionType_; // "press" или "release" (возможно стоит вспомнить как работают enum'ы)
-    string controlName_;
+    std::string actionType_; // "press" или "release" (возможно стоит вспомнить как работают enum'ы)
+    std::string controlName_;
 };
 
 
-int main(){
+int main() {
     std::cout << "Testing KeyboardInput!\n\n";
+
     KeyboardInput press_shift = KeyboardInput("SHIFT", 123.321, "press", "some_ctrl");
     KeyboardInput release_shift = KeyboardInput::createRelease(press_shift, 321.123);
 
-    press_shift.show();
-    std::cout << "\n\n";
-    release_shift.show();
-    std::cout << "\n\npress_shift < release_shift: " << (press_shift < release_shift);
-    std::cout << "\npress_shift == release_shift: " << (press_shift == release_shift);
-    
+    assert(press_shift < release_shift);
+    assert(!(press_shift == release_shift));
 
-    // полиморфизм пока никак не проверить
+    // Тестирование полиморфизма
     InputAction* ptr = &press_shift;
+    assert(ptr->getType() == "KeyboardInput");
+
+    std::cout << "All tests passed successfully!\n";
 
     return 0;
 }
