30b91a279a9349b89ed6c3a5eb02aacadbcf0e7d
Максим Щучкин <maxventilator@gmail.com>
2024-12-02 23:22:11 +0300

    23

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <set>
 #include <utility>
 #include <algorithm>
+#include <memory>
 
 
 enum class InputType {
@@ -42,7 +43,6 @@ class ControlElement {
 class Input {
   public:
     virtual ~Input() = default;
-    virtual InputType getType() const = 0;
     
     bool operator<(const Input& other) const {
       return actionTime_ < other.actionTime_;
@@ -60,9 +60,10 @@ class Input {
       return actionTime_;
     }
 
-    // TODO
-    // void executeAction(){}
-    // void print(){}
+    virtual void executeAction() const = 0;
+
+    virtual void print() const = 0;
+
 
   protected:
     double actionTime_;
@@ -79,12 +80,18 @@ class KeyboardInput: public Input {
       controlElement_ = controlElement;
     }
 
-    InputType getType() const override {
-      return InputType::KeyboardInput;
-    }
-
     static KeyboardInput createRelease(const KeyboardInput& pressAction, double releaseTime){
       return KeyboardInput(pressAction.keyName_, releaseTime, ActionType::Release, pressAction.controlElement_);
+    }
+
+    void executeAction() const override {
+      if (controlElement_ && controlElement_->reactsTo(InputType::KeyboardInput)) {
+        std::cout << "Keyboard action executed: " << keyName_ << " at time " << actionTime_ << "\n";
+      }
+    }
+
+    void print() const override {
+      std::cout << "KeyboardInput: KeyName=" << keyName_ << ", ActionTime=" << actionTime_ << ", ActionType=" << (actionType_ == ActionType::Press ? "Press" : "Release") << "\n";
     }
 
   private:
@@ -101,8 +108,14 @@ class MouseInput: public Input {
       controlElement_ = controlElement;
     }
 
-    InputType getType() const override {
-      return InputType::MouseInput;
+    void executeAction() const override {
+      if (controlElement_ && controlElement_->reactsTo(InputType::MouseInput)) {
+        std::cout << "Mouse action executed: Button=" << keyNumber_ << " at time " << actionTime_ << "\n";
+      }
+    }
+
+    void print() const override {
+      std::cout << "MouseInput: Button=" << keyNumber_ << ", ActionTime=" << actionTime_ << ", ActionType=" << (actionType_ == ActionType::Press ? "Press" : "Release") << "\n";
     }
 
   private:
@@ -130,8 +143,18 @@ class TouchInput: public Input {
       fingersCount_ = fingersCords.size();
     }
 
-    InputType getType() const override {
-      return InputType::TouchInput;
+    void executeAction() const override {
+      if (controlElement_ && controlElement_->reactsTo(InputType::TouchInput)) {
+        std::cout << "Touch action executed with " << fingersCount_ << " fingers at time " << actionTime_ << "\n";
+      }
+    }
+
+    void print() const override {
+      std::cout << "TouchInput: ActionTime=" << actionTime_ << ", FingersCount=" << fingersCount_ << ", Coordinates=";
+      for (const auto& coord : fingersCords_) {
+        std::cout << " [Start(" << coord.start.first << ", " << coord.start.second << ") -> End(" << coord.end.first << ", " << coord.end.second << ")]";
+      }
+      std::cout << "\n";
     }
 
   private:
@@ -143,15 +166,15 @@ class ActionsQueue {
   public:
     ActionsQueue(): actions_({}) {}
 
-    void addAction(Input* action) {
+    void addAction(std::shared_ptr<Input> action) {
       actions_.push_back(action);
-      std::sort(actions_.begin(), actions_.end(), [](const Input* a, const Input* b) {
+      std::sort(actions_.begin(), actions_.end(), [](const std::shared_ptr<Input>& a, const std::shared_ptr<Input>& b) {
         return a->getActionTime() < b->getActionTime();
       });
     }
 
-    std::vector<Input*> getActionsForControl(const std::string& controlName, double startTime = 0.0, double endTime = std::numeric_limits<double>::max()) const {
-      std::vector<Input*> result;
+    std::vector<std::shared_ptr<Input>> getActionsForControl(const std::string& controlName, double startTime = 0.0, double endTime = std::numeric_limits<double>::max()) const {
+      std::vector<std::shared_ptr<Input>> result;
       for (const auto& action : actions_) {
         if (action->getControl()->getName() == controlName && action->getActionTime() >= startTime && action->getActionTime() <= endTime) {
           result.push_back(action);
@@ -163,42 +186,76 @@ class ActionsQueue {
     void executeActions() const {
       std::cout << std::endl;
       for (const auto& action : actions_) {
-        const ControlElement* control = action->getControl();
-        if (control->reactsTo(action->getType())){
-          std::cout << "Executing action on control '" << control->getName() << "' at time " << action->getActionTime() << std::endl;
-        }
+        action->executeAction();
       }
     }
 
   private:
-    std::vector<Input*> actions_;
+    std::vector<std::shared_ptr<Input>> actions_;
 };
 
 
 
 int main() {
-  ControlElement btn1("button1", {InputType::KeyboardInput, InputType::MouseInput});
-  ControlElement btn2("button2", {InputType::KeyboardInput, InputType::MouseInput, InputType::TouchInput});
-  Input* keyPress = new KeyboardInput("A", 1.0, ActionType::Release, &btn1);
-  Input* keyRelease = new KeyboardInput("A", 2.0, ActionType::Press, &btn1);
-  Input* mousePress = new MouseInput(1, 3.0, ActionType::Press, &btn2);
-  Input* touchGesture = new TouchInput(6.0, std::vector<FingerCoordinates>({FingerCoordinates(0, 0, 1, 1)}), &btn2);
+  // Test ControlElement class
+  ControlElement button("PlayButton", {InputType::KeyboardInput, InputType::MouseInput});
+  assert(button.getName() == "PlayButton");
+  assert(button.reactsTo(InputType::KeyboardInput) == true);
+  assert(button.reactsTo(InputType::TouchInput) == false);
+  button.addAction(InputType::TouchInput);
+  assert(button.reactsTo(InputType::TouchInput) == true);
 
-  ActionsQueue actionQueue;
-  actionQueue.addAction(keyPress);
-  actionQueue.addAction(keyRelease);
-  actionQueue.addAction(mousePress);
-  actionQueue.addAction(touchGesture);
+  // Test KeyboardInput
+  ControlElement keyboardControl("KeyboardControl", {InputType::KeyboardInput});
+  KeyboardInput keyPress("A", 1.0, ActionType::Press, &keyboardControl);
+  KeyboardInput keyRelease = KeyboardInput::createRelease(keyPress, 2.0);
+  assert(keyPress.getControl()->getName() == "KeyboardControl");
+  assert(keyPress.getActionTime() == 1.0);
+  assert(keyRelease.getActionTime() == 2.0);
 
-  auto actions = actionQueue.getActionsForControl("button1", 1.0, 5.0);
-  for (const auto& action : actions) {
-    std::cout << "Action on control " << action->getControl()->getName() << " at time " << action->getActionTime() << '\n';
+  // Test MouseInput
+  ControlElement mouseControl("MouseControl", {InputType::MouseInput});
+  MouseInput mousePress(1, 1.5, ActionType::Press, &mouseControl);
+  assert(mousePress.getControl()->getName() == "MouseControl");
+  assert(mousePress.getActionTime() == 1.5);
+
+  // Test TouchInput
+  ControlElement touchControl("TouchControl", {InputType::TouchInput});
+  std::vector<FingerCoordinates> fingers = {
+      FingerCoordinates(0.0, 0.0, 1.0, 1.0),
+      FingerCoordinates(1.0, 1.0, 2.0, 2.0)
+  };
+  TouchInput touch(2.5, fingers, &touchControl);
+  assert(touch.getControl()->getName() == "TouchControl");
+  assert(touch.getActionTime() == 2.5);
+
+  // Test polymorphism
+  std::vector<std::shared_ptr<Input>> inputs;
+  inputs.push_back(std::make_shared<KeyboardInput>(keyPress));
+  inputs.push_back(std::make_shared<MouseInput>(mousePress));
+  inputs.push_back(std::make_shared<TouchInput>(touch));
+
+  for (const auto& input : inputs) {
+      input->print();
   }
+
+  // Test ActionsQueue
+  ActionsQueue actionQueue;
+  actionQueue.addAction(std::make_shared<KeyboardInput>(keyPress));
+  actionQueue.addAction(std::make_shared<MouseInput>(mousePress));
+  actionQueue.addAction(std::make_shared<TouchInput>(touch));
+
+  std::vector<std::shared_ptr<Input>> retrievedActions = actionQueue.getActionsForControl("MouseControl", 0.0, 2.0);
+  assert(retrievedActions.size() == 1);
+  assert(retrievedActions[0]->getActionTime() == 1.5);
 
   actionQueue.executeActions();
 
+  std::cout << "All tests passed!" << std::endl;
+
   return 0;
 }
+
 
 
 
